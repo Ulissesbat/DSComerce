08419201fc3e1e9bf5f0b62e47143b6db60999a0
Ulisses Batista Santana <santana.gru@gmail.com>
2024-07-26 21:11:41 -0300

    Get user logged

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscommerce.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.UserDto;
+import com.devsuperior.dscommerce.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDto> findMe() {
+        UserDto dto = service.getMe();
+        return ResponseEntity.ok(dto);
+    }
+}
