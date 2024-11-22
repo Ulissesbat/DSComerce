9beabf765588a2fcf490201e50fd6b63bd761ae9
Ulisses Batista Santana <santana.gru@gmail.com>
2024-11-22 20:53:06 -0300

    audit data

--- src/main/java/com/devsuperior/dscommerce/entities/Category.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Category.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
 
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -14,6 +15,10 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updateAt;
 
     @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
@@ -44,6 +49,22 @@ public class Category {
 
     public Set<Product> getProducts() {
         return products;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdateAt() {
+        return updateAt;
+    }
+    @PrePersist
+    public void prePersit(){
+        createdAt = Instant.now();
+    }
+    @PreUpdate
+    public void preUpdate(){
+        updateAt = Instant.now();
     }
 
     @Override
